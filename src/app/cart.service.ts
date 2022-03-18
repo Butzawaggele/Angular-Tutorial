f1d3855488a79812a3186846665dffc27078df18
Butzawaggele <stanullo2@gmx.de>
2022-03-18 20:45:47 +0100

    Added basic cart component

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -2,25 +2,21 @@ import { Injectable } from '@angular/core';
 import { Product } from './products';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
   items: Product[] = [];
 
-  export class CartService {
-    items: Product[] = [];
-  /* . . . */
-  
-    addToCart(product: Product) {
-      this.items.push(product);
-    }
-  
-    getItems() {
-      return this.items;
-    }
-  
-    clearCart() {
-      this.items = [];
-      return this.items;
-    }
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
